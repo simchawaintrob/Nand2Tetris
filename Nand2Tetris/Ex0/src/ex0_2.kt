963f5666ade7bfa72ab50831fd45df9bc0d80fbc
שמחה וינטרוב <s0527113940@gmail.com>
2018-11-01 00:08:04 +0200

    end targil 0

--- Nand2Tetris/Ex0/src/ex0_2.kt
+++ Nand2Tetris/Ex0/src/ex0_2.kt
@@ -1,37 +1,47 @@
 import java.io.File
 import java.io.IOException
 import java.nio.file.Files
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView
 
 
 
-     fun main(args: Array<String>){
-//C:\Users\simcha\Downloads
-        var intNumber:Int = 0;
 
-        //println("enter a library path");
+fun main(args: Array<String>){
 
-        var path: String?  ="C:\\Users\\user\\Downloads\\"
+    var intNumber:Int = 0;
+
+    //println("enter a library path");
+
+    val fr :JFileChooser  = JFileChooser()
+    val fw:FileSystemView  = fr.fileSystemView
+
+    var path: String?  = fw.defaultDirectory.path;
+    path = path + "\\"
+    
+    try {
 
         //Scans all files in a folder
-        File(path).walkTopDown().forEach{
-            if (it.name == "hello.vm")
-                try {
+        File(path ).walkTopDown().forEach {
+            if (it.name == "hello.vm") {
 
-                    var text : List<String> =  Files.readAllLines((it.toPath()));
-                    println(it);
+                var text: List<String> = Files.readAllLines((it.toPath()));
+                println(it);
 
-                    text.forEach { inIt ->
-                        var filePath = path + intNumber++.toString() + ".asm"
+                text.forEach { inIt ->
+                    var filePath = path + intNumber++.toString() + ".asm";
+                    if (inIt.contains("you")) {
                         println(inIt)
-                        File(filePath).writeText(inIt)
                     }
-
-                    // Files.write(Paths.get(it.path), number.toByteArray(), StandardOpenOption.APPEND)
-                    //   intNumber ++;// = (intNumber + 1);
-                    //   number = intNumber.toString();
-                } catch (e: IOException) {
+                    File(filePath).writeText(inIt)
                 }
 
-        }
+            }
 
+
+        }
     }
+    catch (e: IOException) {
+    }
+
+}
